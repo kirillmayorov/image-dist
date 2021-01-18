009ec1a0bc00b24388735b5ae18693613bb16e61
mayorovkirill <kir.may97@gmail.com>
2021-01-18 20:17:02 +0700

    change app

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, render_template, json
 import requests
 import hashlib
 import redis
@@ -6,11 +6,20 @@ import redis
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
-default_name = 'Joe Bloggs'
+default_name = 'Kirill Mayorov'
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def mainpage():
+
+    name = default_name
+    salted_name = salt + name
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
+
+    return render_template("template.html", name=name, name_hash=name_hash)
+
+
+@app.route('/get_info', methods=['GET', 'POST'])
+def get_info():
 
     name = default_name
     if request.method == 'POST':
@@ -18,17 +27,8 @@ def mainpage():
 
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
-    header = '<html><head><title>Identidock</title></head><body>'
-    body = '''<form method="POST">
-              Hello <input type="text" name="name" value="{0}">
-              <input type="submit" value="submit">
-              </form>
-              <p>You look like a:
-              <img src="/monster/{1}"/>
-              '''.format(name, name_hash)
-    footer = '</body></html>'
 
-    return header + body + footer
+    return json.dumps({'name': name, 'name_hash': name_hash})
 
 
 @app.route('/monster/<name>')
@@ -44,4 +44,4 @@ def get_identicon(name):
     return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
